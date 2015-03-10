83209ac436ce757fe426ae2e325a74b9c1a1b17a
mzsfsa <maximkozmey@rambler.ru>
2015-03-10 20:15:31 +0300

    last stupid

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		int val = 5;
-		System.out.println("Hello, World!");
+		System.out.println("Hello, World! So Next change");
 	}
 
 }

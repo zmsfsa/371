cb0e5d070737e9ceeeeb792330bdd9ce7fd4cb12
MZmey <mzmey37@mail.ru>
2015-04-21 21:31:09 +0300

    MavenMakHeloWorld

--- pr20_04_2015/gen/org/janusproject/pr20_04_2015/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.janusproject.pr20_04_2015;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- pr20_04_2015/src/pr20_04_2015/HelloWorldAgent.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorldAgent extends Agent{
-	public Status live() {
-		print("Hello world!");
-		return null;
-	}
-}

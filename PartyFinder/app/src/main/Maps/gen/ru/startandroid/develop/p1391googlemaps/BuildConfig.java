d72da3daf966d0b4d2619192e30c29f1e55b75df
Fedot <sfeaal@gmail.com>
2015-05-05 00:41:03 +0400

    Maps

---
+++ PartyFinder/app/src/main/Maps/gen/ru/startandroid/develop/p1391googlemaps/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ru.startandroid.develop.p1391googlemaps;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}

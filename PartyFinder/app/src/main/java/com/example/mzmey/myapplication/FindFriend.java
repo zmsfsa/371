b2a2899952827342c5febdfd9e6a2ce41fd302f2
MZmey <mzmey37@mail.ru>
2015-05-30 00:55:14 +0300

    rendering

--- PartyFinder/app/src/main/java/com/example/mzmey/myapplication/FindFriend.java
+++ PartyFinder/app/src/main/java/com/example/mzmey/myapplication/FindFriend.java
@@ -1,7 +1,14 @@
 package com.example.mzmey.myapplication;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by MZmey on 30.05.2015.
  */
-public class FindFriend {
+public class FindFriend extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.find_friends);
+    }
 }
--- PartyFinder/app/src/main/java/com/example/mzmey/myapplication/Participants.java
+++ PartyFinder/app/src/main/java/com/example/mzmey/myapplication/Participants.java
@@ -1,8 +1,16 @@
 package com.example.mzmey.myapplication;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by MZmey on 30.05.2015.
  */
-public class Participants {
+public class Participants extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.participants);
+    }
 
 }
